fd8c37877e8aa87dd1710440879fc7afa6994d00
Wise <wisebeanss1252@gmail.com>
2026-08-18 14:21:18 +0800

    Fixed UI

--- Game.cpp
+++ Game.cpp
@@ -12,7 +12,10 @@ void Game::Init() {
 void Game::Run() {
 	while (true) {
 		char letter = _getch();
+		int oldY = player.getY();
+		int oldX = player.getX();
 		player.move(letter);
+		map.updateMap(oldY, oldX, ' ');
 		map.updateMap(player.getY(), player.getX(), player.getSymbol());
 		map.printMap();
 	}
