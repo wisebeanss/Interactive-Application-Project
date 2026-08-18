493f7f00023f1d9f0ad3e0e8f6b50eb04787bc4a
bestie <ashes100509@gmail.com>
2026-08-18 11:59:14 +0800

    map changes

--- Map.h
+++ Map.h
@@ -3,9 +3,14 @@
 class Map
 {
 private:
-
+	int carriageNum;
+	char mapSize[7][24];
 public:
 	Map();
+	void buildMap();
+	void printMap() const;
+	void mapTile(int posX, int posY, char symbol);
+	void updateMap(int x, int y, char symbol);
 	~Map();
 };
 
