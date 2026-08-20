96e88ff233c1c85a9c196dab9dd42cee315356f4
CHEONG KAIWEI CLARISS <262423Y@mymail.nyp.edu.sg>
2026-08-20 16:07:27 +0800

    change room 3 puzzle

--- Watch.cpp
+++ Watch.cpp
@@ -39,45 +39,6 @@ Watch::~Watch()
     }
 }
 
-void Watch::getUIBuffer(string buffer[15]) const
-{
-    const int width = 22;
-
-    buffer[0] = "         |    |";
-    buffer[1] = "         |    |";
-    buffer[2] = "+----------------------+";
-
-    int row = 3;
-    buffer[row++] = "|" + centerWatchText("WATCH ", width) + "|";
-    size_t start = 0;
-    while (row < 9 && start < text.size())
-    {
-        size_t end = text.find('\n', start);
-        if (end == string::npos)
-        {
-            end = text.size();
-        }
-
-        string line = text.substr(start, end - start);
-        buffer[row++] = "|" + centerWatchText(line, width) + "|";
-        start = end + 1;
-    }
-
-    while (row < 9)
-    {
-        buffer[row++] = "|" + centerWatchText("", width) + "|";
-    }
-
-    buffer[9] = "+----------------------+";
-    buffer[10] = "         |    |";
-    buffer[11] = "         |    |";
-
-    // Clear unused rows
-    for (int i = 12; i < 15; i++)
-    {
-        buffer[i] = "";
-    }
-}
 
 void Watch::use()
 {
