18360315f92201c47892fb92a12aeee4b8cc314a
CHEONG KAIWEI CLARISS <262423Y@mymail.nyp.edu.sg>
2026-08-20 12:45:36 +0800

    made phone

--- Phones.cpp
+++ Phones.cpp
@@ -1,1 +1,65 @@
 #include "Phones.h"
+
+
+std::string centerPhoneText(const std::string& text, int width)
+{
+    int padding = width - static_cast<int>(text.size());
+
+    if (padding <= 0)
+    {
+        return text.substr(0, width);
+    }
+
+    int left = padding / 2;
+    int right = padding - left;
+
+    return std::string(left, ' ') + text + std::string(right, ' ');
+}
+
+vector<int> Phones::Ids;
+
+Phones::Phones(string text, int X, int Y, int id)
+    : InteractiveObject("Phone", id, false)
+{
+    this->text = text;
+
+    Ids.push_back(id);
+
+    setX(X);
+    setY(Y);
+}
+
+Phones::~Phones()
+{
+    for (size_t i = 0; i < Ids.size(); i++)
+    {
+        if (Ids.at(i) == getId())
+        {
+            Ids.erase(Ids.begin() + i);
+            break;
+        }
+    }
+}
+
+void Phones::use()
+{
+    const int width = 22;
+
+    cout << "+----------------------+" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("PHONE ", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText(text, width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "|" << centerPhoneText("", width) << "|" << endl;
+    cout << "+----------------------+" << endl;
+}
