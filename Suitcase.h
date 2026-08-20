c347212e6f2dbaa509b454b167ea7ffb6240ab29
LIN BOYU <264118A@mymail.nyp.edu.sg>
2026-08-20 15:30:58 +0800

    suitcase

--- Suitcase.h
+++ Suitcase.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "InteractiveObject.h"
-class Suitcase :
-    public InteractiveObject
-{
-};
 
+class Suitcase : public InteractiveObject
+{
+private:
+    bool photo1 = false;
+    bool photo2 = false;
+    bool photo3 = false;
+    bool unlocked = false;
+
+public:
+    Suitcase(int x, int y, int id);
+    void collectPhoto(int num);
+    bool hasAllPhotos();
+    bool tryUnlock();
+    bool isUnlocked() const { return unlocked; }
+}
